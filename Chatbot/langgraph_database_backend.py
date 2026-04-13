9495601e256977fbd9f8970ffa3ac55e0bceff2f
Sudhanshud98 <sudhanshud98@gwu.edu>
2026-04-13 17:42:43 +0530

    Project

--- Chatbot/langgraph_database_backend.py
+++ Chatbot/langgraph_database_backend.py
@@ -1,10 +1,12 @@
+from langgraph.checkpoint import sqlite
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Literal, Annotated
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 load_dotenv()
 
@@ -21,8 +23,9 @@ def chat_node(state : ChatbotState):
 
     return {'messages' : [response]}
 
+conn = sqlite3.connect(database = 'chatbot.db', check_same_thread = False)
 
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn =conn)
 
 
 graph = StateGraph(ChatbotState)
@@ -32,3 +35,10 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 workflow = graph.compile(checkpointer = checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
